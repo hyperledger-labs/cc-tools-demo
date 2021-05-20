098f1427458fa8b9b24d162ac558e8848b2e92d3
Marcos Sarres <marcos.sarres@goledger.com.br>
2021-05-20 18:52:34 -0300

    Fabric-samples installed at /home/msarres

--- installPreReqUbuntu.sh
+++ installPreReqUbuntu.sh
@@ -2,9 +2,7 @@ printf "Installing Building Artefacts\n\n"
 
 sudo apt -y update
 
-sudo apt -y upgrade
-
-sudo apt install build-essential
+sudo apt -y install build-essential
 
 printf "\n\nInstalling GoLang\n"
 
@@ -13,6 +11,8 @@ sudo curl -fsSL https://golang.org/dl/go1.15.12.linux-amd64.tar.gz --output go1.
 sudo rm -rf /opt/go
 
 sudo tar -C /opt -xvzf go1.15.12.linux-amd64.tar.gz
+
+mkdir -p $HOME/go
 
 printf "\n\nInstalling NodeJs\n"
 
@@ -44,6 +44,10 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-
 
 sudo chmod +x /usr/local/bin/docker-compose
 
+rm -f go1.15.12.linux-amd64.tar.gz nodesource_setup.sh get-docker.sh
+
+cd $HOME
+
 printf "\n\nInstalling Hyperledger Fabric binaries and images\n"
 
 curl -sSL http://bit.ly/2ysbOFE | bash -s -- 1.4.12 1.4.9 0.4.22
@@ -52,14 +56,14 @@ printf "\n\nCustomizing enviroment variables\n"
 
 echo "export PATH=$PATH:/opt/go/bin:$HOME/fabric-samples/bin" >> ~/.bashrc
 
-echo "export GOPATH=$HOME" >> ~/.bashrc
+echo "export GOPATH=$HOME/go" >> ~/.bashrc
 
 echo "export GOROOT=/opt/go" >> ~/.bashrc
 
 source ~/.bashrc
 
-rm -f go1.15.12.linux-amd64.tar.gz nodesource_setup.sh get-docker.sh
-
 printf "\n\nEnviroment configured\n"
 
 END
+
+newgrp docker
