3ddab8f445dcc51291314012fceadc7e80f2645b
Bruno Andreghetti <brunoandreghetti@gmail.com>
2021-07-13 15:12:20 -0300

    Conditionally vendor chaincode deps

--- startDev2.sh
+++ startDev2.sh
@@ -5,6 +5,9 @@ docker rmi $(docker images --quiet --filter "dangling=true")
 docker volume rm $(docker volume ls -qf dangling=true)
 
 # Script used to start the development environment.
+if [ ! -d "chaincode/vendor" ]; then
+    cd ./chaincode; go mod vendor; cd ..
+fi
 cd ./chaincode; go fmt ./...; cd ..
 cd ./fabric2; ./startDev.sh; cd ..
 cd ./rest-server; ./startDev2.sh; cd ..
