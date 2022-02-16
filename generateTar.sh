5a0031ecafc26a5771d21c24ecdadba287b2cb6c
Samuel <samuel.venzi@me.com>
2022-02-16 17:05:57 -0300

    Improve arg positions to work on macOS

--- generateTar.sh
+++ generateTar.sh
@@ -7,4 +7,4 @@ cd chaincode && go mod vendor && cd ..
 tar -czf cc-tools-demo.tar.gz chaincode
 
 # Compress file with rest-server (GoFabric will use the one provided)
-# tar -czf --exclude=node_modules cc-tools-demo.tar.gz chaincode rest-server
+# tar -c --exclude=node_modules -zf cc-tools-demo.tar.gz chaincode rest-server
