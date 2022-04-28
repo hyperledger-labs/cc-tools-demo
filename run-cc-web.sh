9bac2d001487c27e5873edc005cf039ad63528ae
Samuel <samuel.venzi@me.com>
2022-04-28 18:20:40 -0300

    Quality of life change, now GoInitus has a docker-compose file

--- run-cc-web.sh
+++ run-cc-web.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 1 ] ; then
-  PORT="8080"
-else
-  PORT=$1
-fi
-
-docker run -p 0.0.0.0:${PORT}:80/tcp --name cc-web${PORT} goledger/cc-webclient:latest
+docker-compose -f rest-server/docker-compose-goinitus.yaml up -d
