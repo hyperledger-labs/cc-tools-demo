aa7bfdfe8ea680d5bca9d9ef3df06c54e78eaac1
mikaellafs <mikaellaferreira0@gmail.com>
2022-06-23 10:47:53 -0300

    Implement theResponseShouldMatchJson and thereIsARunningTestNetwork

--- chaincode/tests/request_test.go
+++ chaincode/tests/request_test.go
@@ -1,7 +1,18 @@
 package tests
+
 import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os/exec"
+	"reflect"
+
 	"github.com/cucumber/godog"
 )
+
+type bodyCtxKey struct{}
+
 func iMakeARequestToWith(arg1, arg2 string, arg3 *godog.DocString) error {
 	return godog.ErrPending
 }
@@ -10,12 +21,52 @@ func theResponseCodeShouldBe(arg1 int) error {
 	return godog.ErrPending
 }
 
-func theResponseShouldMatchJson(arg1 *godog.DocString) error {
-	return godog.ErrPending
+func theResponseShouldMatchJson(ctx context.Context, body *godog.DocString) error {
+	// Get 'ResponseBody' from context
+	respBody, ok := ctx.Value(bodyCtxKey{}).([]byte)
+	if !ok {
+		return errors.New("unavailable context")
+	}
+
+	var expected interface{}
+	var received interface{}
+
+	if err := json.Unmarshal([]byte(body.Content), &expected); err != nil {
+		return err
+	}
+	if err := json.Unmarshal(respBody, &received); err != nil {
+		return err
+	}
+
+	if !reflect.DeepEqual(expected, received) {
+		var expectedBytes []byte
+		var receivedBytes []byte
+		var err error
+		if expectedBytes, err = json.MarshalIndent(expected, "", "  "); err != nil {
+			return err
+		}
+		if receivedBytes, err = json.MarshalIndent(received, "", "  "); err != nil {
+			return err
+		}
+
+		return fmt.Errorf("RECEIVED json:\n%s\ndoes not match EXPECTED:\n%s", string(receivedBytes), string(expectedBytes))
+	}
+
+	return nil
 }
 
 func thereIsARunningTestNetwork(arg1 string) error {
-	return godog.ErrPending
+	// Start test network
+	cmd := exec.Command("../../startDev.sh")
+
+	_, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	return nil
 }
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
