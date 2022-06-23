49f17b3079f7382d223394a49c0eec36fe382b68
André Macedo <andremacedo.pv@gmail.com>
2022-06-23 10:42:59 -0300

    :white_check_mark: Create Request With and Response Code methods for godog
    Create godog auxiliary methods iMakeARequestToWith, that takes a http method, endpoit and json body and make a request with it, and theResponseCodeShouldBe, that verifies if the http status code match the expected one

--- chaincode/tests/request_test.go
+++ chaincode/tests/request_test.go
@@ -1,13 +1,76 @@
 package tests
+
 import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	"github.com/cucumber/godog"
 )
-func iMakeARequestToWith(arg1, arg2 string, arg3 *godog.DocString) error {
-	return godog.ErrPending
+
+// Key Structs definitions
+type bodyCtxKey struct{}
+type statusCtxKey struct{}
+
+func iMakeARequestToWith(ctx context.Context, method, endpoint string, reqBody *godog.DocString) (context.Context, error) {
+	var res *http.Response
+	var err error
+
+	// Initialize http client
+	client := &http.Client{}
+
+	dataAsBytes := bytes.NewBuffer([]byte(reqBody.Content))
+
+	// Create request
+	req, err := http.NewRequest(method, "http://localhost:8080"+endpoint, dataAsBytes)
+	if err != nil {
+		return ctx, err
+	}
+
+	// Set header and make request
+	req.Header.Set("Content-Type", "application/json")
+	res, err = client.Do(req)
+	if err != nil {
+		return ctx, err
+	}
+
+	// Get status code and response body
+	statusCode := res.StatusCode
+	resBody, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return ctx, err
+	}
+	res.Body.Close()
+
+	// Append status code and response body to context
+	bodyCtx := context.WithValue(ctx, bodyCtxKey{}, resBody)
+	statusCtx := context.WithValue(bodyCtx, statusCtxKey{}, statusCode)
+	return statusCtx, nil
 }
 
-func theResponseCodeShouldBe(arg1 int) error {
-	return godog.ErrPending
+func theResponseCodeShouldBe(ctx context.Context, expectedCode int) (context.Context, error) {
+	// Get Status Code from context
+	statusCode, ok := ctx.Value(statusCtxKey{}).(int)
+	if !ok {
+		return ctx, errors.New("context unaveilable while retrieving status")
+	}
+
+	if statusCode != expectedCode {
+		// Get Response body from context
+		resBody, ok := ctx.Value(bodyCtxKey{}).([]byte)
+		if !ok {
+			return ctx, errors.New("unaveilable context while retrieving body")
+		}
+
+		// Test Failed
+		return ctx, fmt.Errorf("received wrong status response. Got %d Expected: %d\nResponse body: %s", statusCode, expectedCode, string(resBody))
+	}
+
+	return ctx, nil
 }
 
 func theResponseShouldMatchJson(arg1 *godog.DocString) error {
