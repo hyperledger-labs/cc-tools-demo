f328cc9e5b59e09f079551a3cde5b0ff75317fd6
André Macedo <andremacedo.pv@gmail.com>
2022-06-27 11:47:18 -0300

    :hammer: Update upgradeCC script to allow 1 to 3 organizations

--- chaincode/assettypes/person.go
+++ chaincode/assettypes/person.go
@@ -18,7 +18,7 @@ var Person = assets.AssetType{
 			IsKey:    true,
 			Tag:      "id",
 			Label:    "CPF (Brazilian ID)",
-			DataType: "cpf",               // Datatypes are identified at datatypes folder
+			DataType: "cpf",                          // Datatypes are identified at datatypes folder
 			Writers:  []string{`org1MSP`, "$orgMSP"}, // This means only org1 can create the asset (others can edit)
 		},
 		{
