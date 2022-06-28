c174fc0d60c3eddbee131ea6140bba5f6dfe386d
mikaellafs <mikaellaferreira0@gmail.com>
2022-06-28 12:04:57 -0300

    Update fabric startDev script to allow 1 or 3 orgs

--- startDev.sh
+++ startDev.sh
@@ -8,6 +8,21 @@ docker volume rm $(docker volume ls -qf dangling=true)
 if [ ! -d "chaincode/vendor" ]; then
     cd ./chaincode; go mod vendor; cd ..
 fi
+
+while test $# -gt 0; do
+  case "$1" in
+      -n)
+        shift
+        NUMORGS=$1
+        shift
+        ;;
+      *)
+        errorln "Unknown flag: $1"
+        exit(1)
+        ;;
+  esac
+done
+
 cd ./chaincode; go fmt ./...; cd ..
-cd ./fabric; ./startDev.sh; cd ..
-cd ./rest-server; ./startDev.sh; cd ..
+cd ./fabric; ./startDev.sh -n ${NUMORGS}; cd ..
+cd ./rest-server; ./startDev.sh -n ${NUMORGS}; cd ..
