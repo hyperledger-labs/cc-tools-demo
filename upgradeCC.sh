5bd01f787bd6fd7e92567db13abaf01c4b27755c
Samuel Venzi <samuel.venzi@me.com>
2023-02-27 19:05:44 -0300

    Fix upgradeCC for 1 org

--- upgradeCC.sh
+++ upgradeCC.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 2 ] ; then
-    printf 'Usage: ./upgradeCC.sh <version> <sequence>\n'
-    exit
+if [[ $# -lt 2 || $# -gt 4 ]]; then
+    printf 'Usage: ./upgradeCC.sh <version> <sequence> [-n <org-quantity>]\n'
+    exit 1
+fi
+
+ORG_QNTY=3
+
+if [[ $# -eq 4 && "$3" == "-n" ]]; then
+    ORG_QNTY=$4
+fi
+
+if [ $ORG_QNTY != 3 -a $ORG_QNTY != 1 ]
+then
+  echo 'WARNING: The number of organizations allowed is either 3 or 1.'
+  echo 'Defaulting to 3 organizations.'
+  ORG_QNTY=3
+fi
+
+if [ $ORG_QNTY == 1 ]
+then
+  CCCG_PATH="../chaincode/collections2-org.json"
+else
+  CCCG_PATH="../chaincode/collections2.json"
 fi
 
 cd ./chaincode; go fmt ./...; cd ..
@@ -11,5 +31,5 @@ version=$1
 sequence=$2
 
 cd fabric
-./network.sh deployCC -ccn cc-tools-demo -ccp ../chaincode -ccl go -ccv $version -ccs $sequence -cci init
+./network.sh deployCC -ccn cc-tools-demo -ccp ../chaincode -ccl go -ccv $version -ccs $sequence -n $ORG_QNTY -cccg $CCCG_PATH -cci init
 cd ..
