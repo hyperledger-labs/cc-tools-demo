3322e26b0ecbacb657387d8b19cf6d37734d062c
mikaellafs <mikaellaferreira0@gmail.com>
2023-08-15 10:21:07 -0300

    Add createAsset benchmark

---
+++ caliper-workspace/workload/createAsset.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+
+class MyWorkload extends WorkloadModuleBase {
+    constructor() {
+        super();
+        this.assets = []
+    }
+
+    async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
+        await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+    }
+
+    async submitTransaction() {
+        const randomId = this.generateRandomString(8)
+        const myArgs = {
+            contractId: this.roundArguments.contractId,
+            contractFunction: 'createAsset',
+            invokerIdentity: 'User1',
+            contractArguments: ['{"asset":[{"@assetType": "benchmarkAsset","id": "'+randomId+'"}]}'],
+            readOnly: false
+        };
+
+        await this.sutAdapter.sendRequests(myArgs);
+        this.assets.push(randomId)
+    }
+
+    async cleanupWorkloadModule() {
+        for (const assetID of this.assets) {
+            console.log(`Worker ${this.workerIndex}: Deleting asset ${assetID}`);
+            const request = {
+                contractId: this.roundArguments.contractId,
+                contractFunction: 'deleteAsset',
+                invokerIdentity: 'User1',
+                contractArguments: ['{"key": {"@assetType":"benchmarkAsset","id":"'+assetID+'"}}'],
+                readOnly: false
+            };
+
+            await this.sutAdapter.sendRequests(request);
+        }
+    }
+
+    generateRandomString(length) {
+        const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let result = "";
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * charset.length);
+            result += charset.charAt(randomIndex);
+        }
+        return result;        
+    }
+}
+
+function createWorkloadModule() {
+    return new MyWorkload();
+}
+
+module.exports.createWorkloadModule = createWorkloadModule;
