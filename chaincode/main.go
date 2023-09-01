6db2b30674457855f154cef1a60a2ef31202f29a
André Macedo <andremacedo.pv@gmail.com>
2023-09-01 10:50:48 -0300

    Startup check on invoke
    Signed-off-by: André Macedo <andremacedo.pv@gmail.com>

--- chaincode/main.go
+++ chaincode/main.go
@@ -18,6 +18,8 @@ import (
 	pb "github.com/hyperledger/fabric-protos-go/peer"
 )
 
+var startupCheckExecuted = false
+
 func SetupCC() error {
 	tx.InitHeader(tx.Header{
 		Name:    header.Name,
@@ -109,10 +111,67 @@ func (t *CCDemo) Init(stub shim.ChaincodeStubInterface) (response pb.Response) {
 	return
 }
 
+func InitFunc(stub shim.ChaincodeStubInterface) (response pb.Response) {
+	// Defer logging function
+	defer logTx(stub, time.Now(), &response)
+
+	if assettypes.DynamicAssetTypes.Enabled {
+		sw := &sw.StubWrapper{
+			Stub: stub,
+		}
+		err := assets.RestoreAssetList(sw, true)
+		if err != nil {
+			response = err.GetErrorResponse()
+			return
+		}
+	}
+
+	err := assets.StartupCheck()
+	if err != nil {
+		response = err.GetErrorResponse()
+		return
+	}
+
+	err = tx.StartupCheck()
+	if err != nil {
+		response = err.GetErrorResponse()
+		return
+	}
+
+	// // Get the args from the transaction proposal
+	// args := stub.GetStringArgs()
+
+	// // Test if argument list is empty
+	// if len(args) != 1 {
+	// 	response = shim.Error("the Init method expects 1 argument, got: " + strings.Join(args, ", "))
+	// 	response.Status = 400
+	// 	return
+	// }
+
+	// // Test if argument is "init" or "upgrade". Fails otherwise.
+	// if args[0] != "init" && args[0] != "upgrade" {
+	// 	response = shim.Error("the argument should be init or upgrade (as sent by Node.js SDK)")
+	// 	response.Status = 400
+	// 	return
+	// }
+
+	response = shim.Success(nil)
+	return
+}
+
 // Invoke is called per transaction on the chaincode.
 func (t *CCDemo) Invoke(stub shim.ChaincodeStubInterface) (response pb.Response) {
 	// Defer logging function
 	defer logTx(stub, time.Now(), &response)
+
+	if !startupCheckExecuted {
+		fmt.Println("Running startup check...")
+		res := InitFunc(stub)
+		if res.Status != 200 {
+			return res
+		}
+		startupCheckExecuted = true
+	}
 
 	var result []byte
 
