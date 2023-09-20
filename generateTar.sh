d15b94b0a3dd68f37f636aff1303e89339a66db3
André Macedo <andremacedo.pv@gmail.com>
2023-09-20 15:07:29 -0300

    Automatic collection generation
    Signed-off-by: André Macedo <andremacedo.pv@gmail.com>

--- generateTar.sh
+++ generateTar.sh
@@ -3,6 +3,7 @@
 # Default values for the flags
 FLAG_CCAPI="none"
 FLAG_LABEL="1.0"
+SKIP_COLL_GEN=false
 
 # You can change this if you want to avoid using the --name flag
 FLAG_NAME="cc-tools-demo"
@@ -32,12 +33,29 @@ while [[ $# -gt 0 ]]; do
                 exit 1
             fi
             ;;
+        --org | -o)
+            if [[ $# -gt 1 ]]; then
+                orgs+=("$2")
+                shift 2
+            else
+                echo "Error: --org flag requires a value."
+                exit 1
+            fi
+            ;;
+        --skip | -s)
+            SKIP_COLL_GEN=true
+            shift 1
+            ;;
         --help | -h)
             echo "Usage: ./generateTar.sh [--ccapi] [--label <label>] [--name <name>]"
             echo "  --ccapi, -c: Include rest-server in the tar file. Default is no ccapi."
             echo "  --help , -h: Show this help message."
             echo "  --label, -l: Label to be used for the chaincode package. Default is 1.0."
             echo "  --name , -n: Name of the chaincode package. Default is ${FLAG_NAME}."
+            echo "  --org  , -o: Include an organization in the collection configuration file"
+            echo "      This option can be used multiple times to include multiple organizations"
+            echo "      If no organizations are specified, the default is to include any organization found in the readers"
+            echo "  --skip , -s: Skip the generation of the collections.json file"
             exit 0
             ;;
         *)
@@ -46,6 +64,17 @@ while [[ $# -gt 0 ]]; do
             ;;
     esac
 done
+
+# Generate collection configuration file
+if [ "$SKIP_COLL_GEN" = false ]
+then
+    if [ ${#orgs[@]} -gt 0 ]
+    then
+        cd ./chaincode; go run . -g --orgs ${orgs[@]}; cd ..
+    else
+        cd ./chaincode; go run . -g; cd ..
+    fi
+fi
 
 # Remove previous tar file
 rm -f ${FLAG_NAME}.tar.gz
@@ -61,7 +90,7 @@ case $FLAG_CCAPI in
     go)
         # Create temporary directory for rest-server
         mkdir -p tmp/rest-server
-        cp -r ccapi/* tmp/rest-server
+        cp -r ccapi/. tmp/rest-server
 
         # Compress file with rest-server (GoFabric will use the one provided)
         tar -c --exclude=vendor -zf ${FLAG_NAME}.tar.gz chaincode.tar.gz -C tmp rest-server
