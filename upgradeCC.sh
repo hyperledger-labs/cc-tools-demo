e43c5b9514d00d961c872a5b9d5f86c09e44b679
Samuel Venzi <samuel.venzi@me.com>
2024-07-10 16:29:51 -0300

    Add ccaas to upgradeCC
    Signed-off-by: Samuel Venzi <samuel.venzi@me.com>

--- upgradeCC.sh
+++ upgradeCC.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/env bash
 
-if [[ $# -lt 2 || $# -gt 4 ]]; then
-    printf 'Usage: ./upgradeCC.sh <version> <sequence> [-n <org-quantity>]\n'
+if [[ $# -lt 2 || $# -gt 6 ]]; then
+    printf 'Usage: ./upgradeCC.sh <version> <sequence> [-n <org-quantity>] [-ccaas <true | false>]\n'
     exit 1
 fi
 
 ORG_QNTY=3
+DEPLOY_CCAAS=false
 SKIP_COLL_GEN=false
 
+version=$1
+sequence=$2
+
 OPTIND=3
-while getopts n:c opt; do
-    case $opt in
-        n)  ORG_QNTY=${OPTARG}
+while [[ $# -ge 1 ]] ; do
+    key="$1"
+    case $key in
+        -n )
+            ORG_QNTY=$2
+            shift
             ;;
-        c)  SKIP_COLL_GEN=true
+        -ccaas )
+            DEPLOY_CCAAS=$2
+            shift
             ;;
-    esac
+        -c )
+            SKIP_COLL_GEN=true
+            ;;
+  esac
+  shift
 done
 
 if [ $ORG_QNTY != 3 -a $ORG_QNTY != 1 ]
@@ -39,9 +52,10 @@ CCCG_PATH="../chaincode/collections.json"
 
 cd ./chaincode; go fmt ./...; cd ..
 
-version=$1
-sequence=$2
-
 cd fabric
-./network.sh deployCC -ccn cc-tools-demo -ccp ../chaincode -ccl go -ccv $version -ccs $sequence -n $ORG_QNTY -cccg $CCCG_PATH -cci init
+if [ "$DEPLOY_CCAAS" = "false" ]; then
+  ./network.sh deployCC -ccn cc-tools-demo -ccp ../chaincode -ccl go -ccv $version -ccs $sequence -n $ORG_QNTY -cccg $CCCG_PATH -cci init
+else
+  ./network.sh deployCCAAS -ccn cc-tools-demo -ccp ../chaincode -ccv $version -ccs $sequence -n $ORG_QNTY -cccg $CCCG_PATH
+fi
 cd ..
