53aec78bd8a98964a5c0fa2450c88a50f4430161
André Macedo <andremacedo.pv@gmail.com>
2026-07-09 16:07:09 -0300

    Bump to 2.5.16
    Signed-off-by: André Macedo <andremacedo.pv@gmail.com>

--- fabric/network.sh
+++ fabric/network.sh
@@ -18,7 +18,7 @@ fi
 export FABRIC_CFG_PATH=${PWD}/configtx
 export VERBOSE=false
 # export COMPOSE_PROJECT_NAME=net
-export IMAGE_TAG=2.5.15
+export IMAGE_TAG=2.5.16
 export SYS_CHANNEL=sys-channel
 
 . scripts/utils.sh
@@ -549,7 +549,7 @@ CC_VERSION="0.1"
 # Chaincode definition sequence
 CC_SEQUENCE=1
 # default image tag
-IMAGETAG="2.5.15"
+IMAGETAG="2.5.16"
 # default ca image tag
 CA_IMAGETAG="latest"
 # default database
